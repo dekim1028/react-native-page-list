e9732aa7b1b9d9b60f880623506308cb34716c8c
dekim1028 <carat.kim@soomgo.com>
2024-05-22 14:03:44 +0900

    remove example

--- example/index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from "react-native";
-import ReactNativePageListExample from "./ReactNativePageListExample";
-import { name as appName } from "./app.json";
-
-AppRegistry.registerComponent(appName, () => ReactNativePageListExample);
